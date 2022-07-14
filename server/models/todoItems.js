4a1d078460309dc3936ccfc096b290da294f08c9
Bineet Sharma <86036476+Bineet24@users.noreply.github.com>
2022-07-14 11:42:48 +0530

    todo-models

---
+++ server/models/todoItems.js
@@ -0,0 +1,21 @@
+//import mongoose to create mongoose model
+const mongoose = require('mongoose');
+
+//create Schema
+const TodoItemSchema = new mongoose.Schema({
+  item:{
+    type:String,
+    required: true
+    
+  }
+  
+ 
+},
+{
+  timestamps:true,
+}
+
+);
+
+//export this Schema
+module.exports = mongoose.model('todo', TodoItemSchema);
